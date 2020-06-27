d2bccacb63091e1d1be676e90d3b5d66eb504e0a
Tarssil <505873988@qq.com>
2020-06-27 17:51:27 +0800

    jcl前端

--- be/res/static/js/add.js
+++ be/res/static/js/add.js
@@ -1,30 +1,23 @@
 //测试提交，对接程序删除即可
 $("#submit").click(function () {
-    var thisUsername = document.getElementById("username").value;
-    var thisPassword = document.getElementById("password").value;
-    if (thisUsername == "") {
-        alert("用户名不能为空!");
+    var filename = document.getElementById("filename").value;
+    if (filename == "") {
+        alert("数据集名不能为空!");
         return false;
-    } else if (thisPassword == "") {
-        alert("密码不能为空!");
-        return false;
-    } else {
+    } else  {
             var param={
                 // 提交时携带的参数
-                "address": "10.60.43.8:8800",
-                "account": thisUsername,
-                "password": thisPassword,
+                "name": filename,
             };
             $.ajax({
                 type: 'POST',
-                url: "http://localhost:20202/login",
+                url: "http://localhost:20202/add",
                 data:JSON.stringify(param) ,
-                success: function(result){
-                    alert("登录成功！"+result);
-                    window.location.href = "index.html";
+                success: function(){
+                    alert("创建成功！");
                 },
                 false:function(result){
-                    alert("登录失败！"+result);
+                    alert("创建失败！"+result);
                 },
                 crossDomain: true,
                 xhrFields: {
---
+++ be/res/static/js/delete.js
@@ -0,0 +1,48 @@
+//测试提交，对接程序删除即可
+$("#submit").click(function () {
+    var filename = document.getElementById("filename").value;
+    if (filename == "") {
+        alert("数据集名不能为空!");
+        return false;
+    } else  {
+        var param={
+            // 提交时携带的参数
+            "name": filename,
+        };
+        $.ajax({
+            type: 'POST',
+            url: "http://localhost:20202/delete",
+            data:JSON.stringify(param) ,
+            success: function(){
+                alert("删除成功！");
+            },
+            false:function(result){
+                alert("删除失败！"+result);
+            },
+            contentType: "application/json;charset=utf-8",
+            crossDomain: true,
+            xhrFields: {
+                withCredentials: true
+            },
+        });
+        // 第一个参数是提交的地址
+        /*$.post("http://localhost:20202/login",
+            {
+                // 提交时携带的参数
+                "address": "10.60.43.8:8800",
+                "account": thisUsername,
+                "password": thisPassword,
+            },
+
+        // 回调函数,data是服务器返回的数据
+            function (data, status) {
+                //var dataObj = eval("(" + data + ")");//转换为json对象
+                alert("登录成功！");
+                window.location.href = "../index.html";
+            }
+            );*/
+
+    }
+});
+
+
